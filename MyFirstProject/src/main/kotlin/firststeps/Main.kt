484b4698ef56b47c50bca6f8fdd8b2cd412760e3
menezesledilson <ledilsonmenezes@gmail.com>
2024-03-06 15:33:38 -0300

    primeiros passos

---
+++ MyFirstProject/src/main/kotlin/firststeps/Main.kt
@@ -0,0 +1,95 @@
+package firststeps
+
+/*fun main() {
+    //var counter: Int
+    // counter = 10
+
+    //var counter: Int = 10
+
+    var counter = 10
+    counter = 5
+    val counter2 = 10
+}*/
+
+//Numeros inteiros
+
+fun main() {
+    /*
+    Byte : 1 byte - 8 bits
+    Short: 2 bytes - 16 bits
+    Int : 4 bytes - 32 bits
+    Long: 8 bytes - 64 bits
+   /*  */
+    val i1: Int = 10
+    val i2: Int = 10
+    val i3: Int
+
+    val l1 = 10L
+    val l2 =120031548622467*/
+
+    /*
+    * Float : 4 bytes - 32 bits
+    * Double : 8 bytes - 64 bits
+    * */
+
+    /*  val d1 = 14.5
+      val d2 : Double = 5.0
+      val f1 = 14.5F
+      val f2: Float = 32.5F*/
+
+    /*Conversão */
+
+    /* val i1 = 10
+     val d1 = i1.toDouble()
+     println(d1)
+
+     val l1 = i1.toLong()
+     println(l1)
+
+     val d2 = 10.34
+     val i2 = d2.toInt()
+     println(i2)
+
+     val i3 = 2000
+     val b1 = i3.toByte()
+     println(b1)*/
+
+
+    /*
+    * Byte-> UByte
+    * Short-> UShort
+    * Int-> UInt
+    * Long-> ULong
+    *     * */
+    /* val x = 100U
+     val y = 100UL
+
+     val x2 = x.toInt()
+     val y2 = y.toDouble()
+
+     val x3 = -20
+     val x4 = x3.toUInt()
+     println(x4)*/
+
+    /*Tipos boolean*/
+    /*val b1: Boolean = true
+    val b2 = false */
+
+    /* tipos de dados char*/
+
+    /*val c: Char = 'A' // um caracter que poder receber seja numero ou letra ou vazio
+    println(c)
+    println(c.code)
+    var i = 65
+    println(i.toChar())
+
+    val c2 = 'a'
+    println(c2.isDigit())
+    println(c2.isUpperCase())
+    println(c2.digitToInt())*/
+
+    val s1 : String = "abc"
+    val s2 = "def"
+    val s1s2 = s1 + s2
+    print (s1s2)
+}
---
+++ MyFirstProject/src/main/kotlin/firststeps/inteiros.kt
@@ -0,0 +1,4 @@
+package firststeps
+
+class inteiros {
+}
