018f6736a3d55faac2e8a02da673292d42d68ffc
unknown <ledilsonmenezes@gmail.com>
2024-04-01 21:23:39 -0300

    secao17

--- secao17/exemplos/src/Main.kt
+++ secao17/exemplos/src/Main.kt
@@ -1,11 +1,37 @@
 fun main() {
-a1()
+    val p1 = Point(10, 5)
+    val p2 = Point(5, 3)
+    val p3 = p1 + p2
+    val p4 = p2 - p3
+    val p5 = p1 * 2
 
-}
-fun a1(){
-    println("a1()")
+    var p6 = Point(0, 0)
+    p6++
+
+    println(p1)
+    println(p2)
+    println(p3)
+    println(p4)
+    println(p5)
+    println(p6)
 }
 
-fun a2(){
-    println("a2()")
+data class Point(val x: Int, val y: Int) {
+
+    operator fun plus(other: Point): Point {
+        return Point(x = x + other.x, y = y + other.y)
+    }
+
+    operator fun minus(other: Point): Point {
+        return Point(x = x - other.x, y = y - other.y)
+    }
+
+    operator fun times(multiplier: Int): Point {
+        return Point(x = x * multiplier, y = y * multiplier)
+    }
+
+    operator fun inc(): Point {
+        return Point(x = x + 1, y = y + 1)
+    }
+
 }
